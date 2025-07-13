cbf20f6b899790e7e8db9040036c320002465721
Muskan Nisar <159582953+muskann12@users.noreply.github.com>
2025-07-13 20:50:51 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Navbar from "./components/navbar";
 import Footer from "./components/footer";
 import TopHeader from "./components/top";
+import WhatsappButton from "./components/WhatsappButton";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -94,6 +95,7 @@ export default function RootLayout({
         <Navbar />
         {children}
         <Footer />
+        <WhatsappButton />
       </body>
     </html>
   );
